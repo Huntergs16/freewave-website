d47944b4bb2d96ab306ca39a8420de78c9b23575
Huntergs16 <samoyhun@cse.msu.edu>
2023-04-11 18:25:14 -0400

    fixing alts

--- app/page.js
+++ app/page.js
@@ -6,16 +6,16 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-between p-6">
       <div className="w-full h-min grid grid-cols-2 grid-rows-auto gap-4">
         <div className="rounded-lg border-2 overflow-hidden h-min">
-          <img src="/FW_header.png" className="object-cover h-full w-full" alt="placeholder image" />
+          <img src="/FW_header.png" className="object-cover h-full w-full" alt="header pic" />
         </div>
         <div className="rounded-lg border-2 overflow-hidden h-min" >
-          <img src="/FWPROMO.jpg" className="object-cover h-full w-full" alt="placeholder image" />
+          <img src="/FWPROMO.png" className="object-cover h-full w-full" alt="promo pic" />
         </div>
         <div className="rounded-lg border-2 overflow-hidden h-min">
-          <img src="/FW_header.png" className="object-cover h-full w-full" alt="placeholder image" />
+          <img src="/FW_header.png" className="object-cover h-full w-full" alt="header pic" />
         </div>
         <div className="rounded-lg border-2 overflow-hidden h-min" >
-          <img src="/FWPROMO.jpg" className="object-cover h-full w-full" alt="placeholder image" />
+          <img src="/FWPROMO.png" className="object-cover h-full w-full" alt="promo pic" />
         </div>
       </div>
     </div>
