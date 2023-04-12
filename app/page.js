15b6d46cb16a5547a4b85e88221c76d929372b9a
Huntergs16 <samoyhun@cse.msu.edu>
2023-04-11 21:33:49 -0400

    padding body altered

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between px-6 py-3">
+    <div className="flex min-h-screen flex-col items-center justify-between p-3">
       <div className="w-full h-min grid grid-cols-2 grid-rows-auto gap-2">
         <div className="rounded-lg border-2 overflow-hidden h-min">
           {/* <img src="/FW_header.png" className="object-cover h-full w-full" alt="header pic" /> */}
