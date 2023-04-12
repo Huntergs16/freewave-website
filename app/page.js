de2025ff5e24caad72138b4a976ceaf375ab9d0f
Huntergs16 <samoyhun@cse.msu.edu>
2023-04-12 00:53:29 -0400

    switched matt_jaymo to png

--- app/page.js
+++ app/page.js
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-3">
       <div className="w-full h-min md:lg:grid md:lg:grid-cols-2 md:lg:grid-rows-auto gap-2 sm:flex sm:overflow-x-scroll">
-        <div className="rounded-lg border-2 overflow-hidden h-min">
-          <Image src="/FW_header.png" className="object-cover h-full w-full" alt="header pic" width={1000} height={1000} />
-        </div>
         <div className="rounded-lg border-2 overflow-hidden h-min" >
           <Image src="/FWPROMO.png" className="object-cover h-full w-full" alt="promo pic" width={1000} height={1000} />
         </div>
