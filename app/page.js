a0fd2d8d2908f31c3fffef971e58956df513c5b5
Huntergs16 <samoyhun@cse.msu.edu>
2023-04-11 21:01:51 -0400

    new pics and clickable confirmed

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-6">
-      <div className="w-full h-min grid grid-cols-2 grid-rows-auto gap-4">
+      <div className="w-full h-min grid grid-cols-2 grid-rows-auto gap-2">
         <div className="rounded-lg border-2 overflow-hidden h-min">
           <img src="/FW_header.png" className="object-cover h-full w-full" alt="header pic" />
         </div>
@@ -12,10 +12,10 @@ export default function Home() {
           <img src="/FWPROMO.png" className="object-cover h-full w-full" alt="promo pic" />
         </div>
         <div className="rounded-lg border-2 overflow-hidden h-min">
-          <img src="/FW_header.png" className="object-cover h-full w-full" alt="header pic" />
+          <img src="/brino7.png" className="object-cover h-full w-full" alt="header pic" />
         </div>
         <div className="rounded-lg border-2 overflow-hidden h-min" >
-          <img src="/FWPROMO.png" className="object-cover h-full w-full" alt="promo pic" />
+          <img src="/nickback.png" className="object-cover h-full w-full" alt="promo pic" />
         </div>
       </div>
     </div>
